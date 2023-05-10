82528350ffc78e1f8b6ae8326b6b17827eb932d5
Hippolyte Nogarede <hnogared@c2r6p4.42perpignan.fr>
2023-05-10 16:08:13 +0200

    moved functions from Srcs/defuser_wizard.c to Srcs/strings_utils.c

--- Includes/defuser_wizard.h
+++ Includes/defuser_wizard.h
@@ -11,6 +11,13 @@
 # include <fcntl.h>
 # include <dirent.h>
 
+/* strings_utils.c file */
 int chrstr(char c, char *str);
+int left_strcmp(char *str1, char *str2);
+
+char    *crop(char *str);
+char    *ft_strjoin(char *dest, char *src);
+
+void    free_str_tab(char **str_tab);
 
 #endif
