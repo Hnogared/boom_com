c41fc332c439cafae7da0aebb8f8dc8cd5db2c72
Hnogared <hnogared@c2r6p4.42perpignan.fr>
2023-05-30 18:29:43 +0200

    minor change in termios structure  management

--- Srcs/Utils/setup_functions.c
+++ Srcs/Utils/setup_functions.c
@@ -80,20 +80,13 @@ struct termios	set_termios_opt(int fd, int baudrate)
 	return (toptions);
 }
 
-void	free_termios(struct termios *toptions)
-{
-	if (!toptions)
-		return ;
-	free(toptions->c_cc);
-	free(toptions);
-}
 
 void	exit_helper(portopts *conn_options, dispopts *disp_options)
 {
 	if (conn_options->fd > -1)
 		close(conn_options->fd);
 	if (conn_options->toptions)
-		free_termios(conn_options->toptions);
+		free(conn_options->toptions);
 	if (conn_options)
 		free(conn_options);
 	if (disp_options)
