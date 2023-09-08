f80eac30d89af3cc262f976c1b7e85cbf716e73e
Hnogared <hnogared@student.42perpignan.fr>
2023-09-08 23:54:17 +0200

    protype to test bit stuffer

--- Srcs/main.c
+++ Srcs/main.c
@@ -70,6 +70,7 @@ int	main(void)
 	attroff(COLOR_PAIR(1));
 
 //	goto_layout_1(&conn_options, &disp_options);
+	open_usb_port(&conn_options);
 	goto_layout_labyrinth(&conn_options, &disp_options);
 	while (1)
 	{
